b9438d817f0760bb7f9d4ea32d782ee9857b9722
Marcus Ureta <silvergreat0@gmail.com>
2025-10-26 10:33:36 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# portfolio-site
+marcus ureta's portfolio site
---
+++ src/Header/Header.jsx
@@ -0,0 +1,22 @@
+import './Header.css'
+
+function Header()
+{
+    return(
+        <header>
+            <h1>Marcus Timothy Ureta</h1>
+            <h2>Programming Student</h2>
+
+            <nav>
+                <a href="">Home</a>
+                <a href="">Projects</a>
+                <a href="">About</a>
+                <a href="">Contact</a>
+            </nav>
+
+            <button>Resume</button>
+        </header>
+    )
+}
+
+export default Header;
